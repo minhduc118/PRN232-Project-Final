bc97498262af6a6608b29a96159e6da31573f9d4
Ortus24 <nhatlckbt007@gmail.com>
2026-07-23 03:24:47 +0700

    feat: configure server infrastructure, dependency injection, and middleware in Program.cs

--- SportCourtManagent_Server/SportCourtManagent_Server/Program.cs
+++ SportCourtManagent_Server/SportCourtManagent_Server/Program.cs
@@ -255,7 +255,12 @@ app.UseSwaggerUI(c =>
 // Redirect root URL directly to Swagger UI
 app.MapGet("/", () => Results.Redirect("/swagger"));
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseCors("AllowAll");
 
